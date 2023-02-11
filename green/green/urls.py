31c17aaac0251fb90859278c01755fa0b7b81cf5
irohan0 <rohan.inamdar222@gmail.com>
2023-02-11 23:18:22 +0530

    auth completed

--- green/green/urls.py
+++ green/green/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Crop Predictor Admin"
+admin.site.site_title = "Crop Predictor Admin Portal"
+admin.site.index_title = "Welcome to Crops Predictor"
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls'))
--- green/home/views.py
+++ green/home/views.py
@@ -1,13 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
 def signup(request):
+    if request.method=='POST':
+        uname=request.POST.get('uname')
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password')
+        my_user=User.objects.create_user(uname,email,pass1)
+        my_user.save()
+        return redirect('login')
+
     return render(request,'signup.html')
 
 def login(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pass1=request.POST.get('pass')
+        user=authenticate(request,username=username,password=pass1)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return redirect('user')
+
     return render(request,'login.html')
 
 def user(request):
