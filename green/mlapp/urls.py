cc97e4ad3f0df277678f40a90ead6942008a4982
irohan0 <rohan.inamdar222@gmail.com>
2023-02-11 23:43:26 +0530

    ml base set

---
+++ green/mlapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from mlapp import views
+
+urlpatterns = [
+    path("",views.pridictor),
+    
+
+]
---
+++ green/mlapp/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def pridictor(request):
+    return render(request,'user.html')
